1640398db8b1b7d18a191c039d441257c759111a
mrhan <849272199@qq.com>
2020-05-21 15:58:08 +0800

    修改fs启动脚本

--- pyscripts/cdr/callcenter/callcenter_init.py
+++ pyscripts/cdr/callcenter/callcenter_init.py
@@ -1,6 +1,10 @@
 import freeswitch
 import ConfigParser
+import sys
+import re
+import time
 from include import function
+from ESL import *
 
 #config.ini
 rootdir = freeswitch.getGlobalVariable("base_dir")
@@ -15,15 +19,138 @@ Agent_record = config.get('webservice','Agent_record')
 def runtime(args):
     freeswitch.msleep(2000)
     freeswitch.consoleLog("INFO","\nCallcenter initializing!\n")
-    api = freeswitch.API()
     params = {}
     params["action"] = "InitSystem"
     msg = function.request_get(WebServiceURL,params)
     if msg:
-        api.execute("pyrun","callcenter.agent_event_thread")
-        freeswitch.consoleLog("INFO","\ncallcenter initializ success!\n")
-        return
+        freeswitch.consoleLog("INFO","\nCallcenter initializ success!\n")
     else:
-        freeswitch.consoleLog("ERR","\nCallcenter initialize failed!\n")
+        freeswitch.consoleLog("ERR","\nCallcenter initializ  failed!\n")
         return
-    
+    api = freeswitch.API()
+    freeswitch.consoleLog("INFO","\nStart listening to CHANNEL_CALLSTATE events\n")
+    con = ESLconnection("127.0.0.1","8024","Clue1234")
+    con.events("plain", "CHANNEL_CALLSTATE")
+    if con.connected:
+        while True:
+            e = con.recvEvent()
+            ch = e.getHeader("Caller-Channel-Name")
+            ccs = e.getHeader("Channel-Call-State")
+            occs = e.getHeader("Original-Channel-Call-State")
+            direction = e.getHeader("Caller-Direction")
+            cna = e.getHeader("Caller-Network-Addr")
+            olna = e.getHeader("Other-Leg-Network-Addr")
+            urlparam = {}
+            answerts = 0
+            ORIGIN_TIME = "1970-01-01 08:00:00"
+            post = False
+            put = False
+            able = False
+              #-------呼出---------
+            if ch != None and re.match(AgentSipIf,ch) and direction =="inbound":
+                #freeswitch.consoleLog("info",e.serialize("text"))
+                agent = e.getHeader("Caller-Caller-ID-Number")
+                uuid = e.getHeader("Caller-Unique-ID")
+                other_number = e.getHeader("Caller-Destination-Number")
+                bleg_uuid = e.getHeader("Other-Leg-Unique-ID")
+                if re.match(Agent_record,agent):
+                    able = True
+                if agent:
+                    if ccs == "RINGING":
+                        urlparam["action"] = "AgentCalloutRinging"
+                        urlparam["agent_name"] = agent
+                        urlparam["uuid"]  = uuid
+                        urlparam["created_datetime"] = function.format_unixtime_from_sec(e.getHeader("Caller-Channel-Created-Time"))
+                        urlparam["other_number"] = other_number
+                        urlparam["bleg_uuid"] = bleg_uuid
+                        urlparam["calling_ip"] = cna
+                        post = True												
+                    elif ccs == "ACTIVE":
+                        urlparam["action"] = "AgentCalloutAndwered"
+                        urlparam["agent_name"] = agent			
+                        urlparam["uuid"] = uuid		
+                        urlparam["other_number"] = other_number
+                        urlparam["bleg_uuid"] = bleg_uuid
+                        urlparam["calling_ip"] = cna
+                        urlparam["called_ip"] = olna
+                        urlparam["created_datetime"] = function.format_unixtime_from_sec(e.getHeader("Caller-Channel-Created-Time"))
+                        urlparam["answered_datetime"] = function.format_unixtime_from_sec(e.getHeader("Caller-Channel-Answered-Time"))
+                        answerts = function.format_unixtime_from_us(e.getHeader("Caller-Channel-Answered-Time"))
+                        put = True					
+                    elif ccs == "HANGUP":				    
+                        urlparam["action"] = "AgentCalloutHangup"
+                        urlparam["agent_name"] = agent		
+                        urlparam["uuid"] = uuid			
+                        urlparam["other_number"] = other_number
+                        urlparam["bleg_uuid"] = bleg_uuid
+                        urlparam["calling_ip"] = cna
+                        urlparam["called_ip"] = olna	
+                        urlparam["created_datetime"] = function.format_unixtime_from_sec(e.getHeader("Caller-Channel-Created-Time"))
+                        urlparam["answered_datetime"] = function.format_unixtime_from_sec(e.getHeader("Caller-Channel-Answered-Time"))
+                        urlparam["hangup_datetime"] = function.format_unixtime_from_sec(e.getHeader("Caller-Channel-Hangup-Time"))
+                        urlparam["hangup_cause"] = e.getHeader("Hangup-Cause")
+                        origin_time = function.format_unixtime_from_date(urlparam["answered_datetime"])
+                        if str(origin_time) == ORIGIN_TIME:
+                            urlparam["answered_datetime"] = urlparam["hangup_datetime"]
+                        put = True
+             #-------呼入---------
+            elif ch != None and re.match(AgentSipIf,ch) and direction =="outbound":
+                #freeswitch.consoleLog("info",e.serialize("text"))
+                agent = e.getHeader("Caller-Callee-ID-Number")
+                uuid = e.getHeader("Caller-Unique-ID")
+                other_number = e.getHeader("Caller-Caller-ID-Number")
+                bleg_uuid = e.getHeader("Other-Leg-Unique-ID")
+                if re.match(Agent_record,agent):
+                    able = True
+                if agent:
+                    if ccs == "RINGING":
+                        urlparam["action"] = "AgentCallinRinging"
+                        urlparam["agent_name"] = agent
+                        urlparam["uuid"]  = uuid
+                        urlparam["calling_ip"] = cna
+                        urlparam["created_datetime"] = function.format_unixtime_from_sec(e.getHeader("Caller-Channel-Created-Time"))
+                        urlparam["other_number"] = other_number
+                        urlparam["bleg_uuid"] = bleg_uuid
+                        post = True											
+                    elif ccs == "ACTIVE":
+                        urlparam["action"] = "AgentCallinAndwered"
+                        urlparam["agent_name"] = agent		
+                        urlparam["uuid"] = uuid		
+                        urlparam["other_number"] = other_number
+                        urlparam["bleg_uuid"] = bleg_uuid
+                        urlparam["calling_ip"] = cna
+                        urlparam["called_ip"] = olna
+                        urlparam["created_datetime"] = function.format_unixtime_from_sec(e.getHeader("Caller-Channel-Created-Time"))
+                        urlparam["answered_datetime"] = function.format_unixtime_from_sec(e.getHeader("Caller-Channel-Answered-Time"))
+                        answerts = function.format_unixtime_from_us(e.getHeader("Caller-Channel-Answered-Time"))
+                        put = True	
+                    elif ccs == "HANGUP":				    
+                        urlparam["action"] = "AgentCallinHangup"
+                        urlparam["agent_name"] = agent		
+                        urlparam["uuid"] = uuid			
+                        urlparam["other_number"] = other_number
+                        urlparam["bleg_uuid"] = bleg_uuid	
+                        urlparam["calling_ip"] = cna
+                        urlparam["called_ip"] = olna
+                        urlparam["created_datetime"] = function.format_unixtime_from_sec(e.getHeader("Caller-Channel-Created-Time"))
+                        urlparam["answered_datetime"] = function.format_unixtime_from_sec(e.getHeader("Caller-Channel-Answered-Time"))
+                        urlparam["hangup_datetime"] = function.format_unixtime_from_sec(e.getHeader("Caller-Channel-Hangup-Time"))
+                        urlparam["hangup_cause"] = e.getHeader("Hangup-Cause")
+                        origin_time = function.format_unixtime_from_date(urlparam["answered_datetime"])
+                        if str(origin_time) == ORIGIN_TIME:
+                            urlparam["answered_datetime"] = urlparam["hangup_datetime"]
+                        put = True
+            if ccs == "ACTIVE" and EnableRecord and able and 0 != answerts:
+                path = rootdir+RecoardPath+time.strftime("%Y-%m-%d",answerts)+"/"+urlparam["agent_name"]+"/"
+                recfile = path+urlparam["uuid"]+"_"+urlparam["agent_name"]+"_"+urlparam["other_number"]+"_"+time.strftime("%Y%m%d%H%M%S",answerts)+".wav"
+                api.execute("uuid_record",urlparam["uuid"]+" start "+recfile)
+            if post and able:
+                msg = function.request_post(WebServiceURL, urlparam)
+                if not msg:
+                    freeswitch.consoleLog("ERR","\n"+msg+"\n")
+            elif put and able:
+                result = function.request_put(WebServiceURL, urlparam)
+                if result != "success":
+                    freeswitch.consoleLog("ERR","\n"+result+"\n")
+        freeswitch.msleep(200)
+    freeswitch.consoleLog("INFO","\nagent event thread exit!\n")
--- pyscripts/cdr/callcenter/test.py
+++ pyscripts/cdr/callcenter/test.py
@@ -1,93 +1,29 @@
-#!/usr/bin/python
-##Polling file
-import time
 import freeswitch
-import re
-import random
+import ConfigParser
+from include import function
 
-rootdir = freeswitch.getGlobalVariable("base_dir");
-api = freeswitch.API();
-file_path = rootdir+"/scripts/strategy/number"
+#config.ini
+rootdir = freeswitch.getGlobalVariable("base_dir")
+config = ConfigParser.SafeConfigParser()
+config.read(rootdir+"/scripts/callcenter/config.ini")
+WebServiceURL = config.get('webservice', 'WebServiceURL')
+AgentSipIf = config.get('webservice', 'AgentSipIf')
+EnableRecord = config.getboolean('webservice', 'EnableRecord')
+RecoardPath = config.get('webservice', 'RecoardPath')
+Agent_record = config.get('webservice','Agent_record')
 
-def file_list(filename):
-    num_list = []
-    file = open(filename,"r")
-    for line in file.readlines():
-        line = line.strip('\n')
-        num_list.append(line)
-    file.close()
-    return num_list
-
-def check_gateway_limits(key, limits, upgw):
-    total_number = 0
-    last_day = 0
-    today = time.strftime("%Y%m%d", time.localtime())
-    if api.executeString("db exists/call_limit_last_count_of_day/data_time") == "false":
-        api.executeString("db insert/call_limit_last_count_of_day/data_time/"+str(today))
+def runtime(args):
+    freeswitch.msleep(2000)
+    freeswitch.consoleLog("INFO","\nCallcenter initializing!\n")
+    api = freeswitch.API()
+    params = {}
+    params["action"] = "InitSystem"
+    msg = function.request_get(WebServiceURL,params)
+    if msg:
+        api.execute("pyrun","callcenter.agent_event_thread")
+        freeswitch.consoleLog("INFO","\ncallcenter initializ success!\n")
+        return
     else:
-        last_day = api.executeString("db select/call_limit_last_count_of_day/data_time")
-    if api.executeString("db exists/call_limit_value_count_of_day/"+str(key)) == "false":
-        api.executeString("db insert/call_limit_value_count_of_day/"+str(key)+"/0")
-    else:
-        count = api.executeString("db select/call_limit_value_count_of_day/"+str(key))
-        total_number = int(count)
-    if today != last_day:
-        total_number = 0
-        api.executeString("db delete/call_limit_value_count_of_day/data_time")
-        for k in range(len(upgw)):
-            api.executeString("db delete/call_limit_value_count_of_day/"+str(upgw[k]))
-        api.executeString("db insert/call_limit_last_count_of_day/data_time/"+str(today))
-    if total_number < int(limits):
-        total_number = total_number + 1
-        api.executeString("db insert/call_limit_value_count_of_day/"+str(key)+"/"+str(total_number))
-        return True
-    else:
-        return False
-
-def handler(session, args):
-    pass
-
-def fsapi(session, stream, env, args):
-    count, fullgw, current_pos, new_pos, idx = 0, 0, 0, 0, 0
-    upgw = []
-    if args:
-        string = re.split('\s+', args)
-    else:
-        freeswitch.consoleLog('ERR', 'must argv1 profile %s\n')
-    argv1 = string[0]
-    upgw_list = file_list(file_path)
-    for gw in range(len(upgw_list)):
-        if re.search(r"State\s+REGED\s+",api.executeString("sofia status gateway "+upgw_list[gw])):
-            upgw.append(upgw_list[gw])
-            count = count + 1
-    if api.executeString("db exists/gateway_route/current_pos") == "false":
-        api.executeString("db insert/gateway_route/current_pos/0")
-    current_pos = api.executeString("db select/gateway_route/current_pos")
-    if int(current_pos) >= count:
-        current_pos = 0
-    new_pos = int(current_pos) + 1
-    idx = int(current_pos)
-    api.executeString("db insert/gateway_route/current_pos/"+str(new_pos))
-    info = api.executeString("show channels")
-    while True:
-        if not re.search("sofia/gateway/"+upgw[idx],info):
-            if check_gateway_limits(upgw[idx],argv1,upgw):
-                stream.write(str(upgw[idx]))
-                freeswitch.consoleLog("info", "Discovery of available gateway: %s\n" % upgw[idx])
-                break
-            else:
-                idx = idx + 1
-                fullgw = fullgw + 1
-                if idx >= count:
-                    idx = 0
-                if fullgw == count:
-                    freeswitch.consoleLog("err", "The number limit has been reached: %s\n" % argv1)
-                    break
-        else:
-            idx = idx + 1
-            fullgw = fullgw + 1
-            if idx >= count:
-                idx = 0
-            if fullgw == count:
-                freeswitch.consoleLog("err", "The number limit has been reached: %s\n" % argv1)
-                break
+        freeswitch.consoleLog("ERR","\nCallcenter initialize failed!\n")
+        return
+    
